9fad9d129f418972cda3c0ea6dadf7ece95a8c61
chahak1707 <pahujamanni1978@gmail.com>
2026-01-04 14:29:35 +0530

    Module3,Lesson5

---
+++ math_module.py
@@ -0,0 +1,7 @@
+import math
+num=float(input("Enter a number:"))
+
+print("Square Root of",num,":",math.sqrt(num))
+print("Power:",math.pow(num,2))
+print("Ceil Value of",num,":",math.ceil(num))
+print("Floor Value of",num,":",math.floor(num))
